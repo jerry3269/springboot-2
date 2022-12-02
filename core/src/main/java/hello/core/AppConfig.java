ee0490b4de7273d89f2fe113bdfe28aa11312c24
jerry3269 <jerry3269@ajou.ac.kr>
2022-12-02 15:31:20 +0900

    스프링 컨테이너와 스프링 빈(수정)

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -17,19 +17,24 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
-    @Bean
-    public DiscountPolicy discountPolicy(){
-        return new RateDiscountPolicy();
-    }
-    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
     @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+
+
 }
